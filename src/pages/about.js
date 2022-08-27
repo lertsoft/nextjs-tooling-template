c846a5fa01749f0e9f5e5b371ecba1fde84ed777
Ronny Coste <ronny21.97@gmail.com>
2022-08-26 22:06:42 -0400

    Javascript Next Js template

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import { Meta } from '../layout/Meta';
+
+const About = () => (
+    <>
+ <Meta title="Lorem ipsum" description="Lorem ipsum" />
+    <p className=" text-color-blue ">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
+      voluptatibus distinctio recusandae autem esse explicabo molestias officia
+      placeat, accusamus aut saepe.
+    </p>
+    </>
+);
+
+export default About;
---
+++ src/utils/AppConfig.js
@@ -0,0 +1,8 @@
+export const AppConfig = {
+    site_name: 'Javascript Template',
+    title: 'Javascript base template to start any project',
+    description:
+      'Javascript based template with NextTS, React, TailwindCSS, lint, Metadata, Folder structure, and quick setup time!',
+    locale: 'en',
+  };
+  
